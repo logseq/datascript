ec937bc2bc932f0e1fdbbc1f67413d55c18ddc4a
Tienson Qin <tiensonqin@gmail.com>
2023-12-08 15:52:46 +0800

    Add cljs storage tests (#1)
    * Run cljs tests using shadow-cljs
    
    Also copied storage test and commented out tests
    that couldn't run b/c fns weren't ported
    
    To run tests:
    clj -A:shadow-cljs:test compile test && node target/datascript.js
    
    * Bump persistent-sorted-set
    
    * fix count tests since cljs has cache
    
    * fix: storage tests
    
    * Clean up deps related to shadow-cljs
    
    * Rename test runner to make sense for datascript
    
    ---------
    
    Co-authored-by: Gabriel Horner <gabriel@logseq.com>

---
+++ test/node_test_runner.cljs
@@ -0,0 +1,47 @@
+(ns node-test-runner
+  (:require [cljs.test :as t]
+            [datascript.test.query]
+            [datascript.test.pull-api]
+            [datascript.test.query-rules]
+            [datascript.test.conn]
+            [datascript.test.explode]
+            [datascript.test.filter]
+            [datascript.test.ident]
+            [datascript.test.query-fns]
+            [datascript.test.query-not]
+            [datascript.test.query-or]
+            [datascript.test.query-aggregates]
+            [datascript.test.query-pull]
+            [datascript.test.query-return-map]
+            [datascript.test.tuples]
+            [datascript.test.query-find-specs]
+            [datascript.test.components]
+            [datascript.test.serialize]
+            [datascript.test.transact]
+            [datascript.test.storage]))
+
+(defmethod t/report [::t/default :end-run-tests] [{:keys [error fail]}]
+  (if (pos? (+ error fail))
+    (js/process.exit 1)
+    (js/process.exit 0)))
+
+(defn init []
+  (t/run-tests 'datascript.test.query
+               'datascript.test.pull-api
+               'datascript.test.query-rules
+               'datascript.test.conn
+               'datascript.test.explode
+               'datascript.test.filter
+               'datascript.test.ident
+               'datascript.test.query-fns
+               'datascript.test.tuples
+               'datascript.test.transact
+               'datascript.test.components
+               'datascript.test.serialize
+               'datascript.test.query-pull
+               'datascript.test.query-find-specs
+               'datascript.test.query-return-map
+               'datascript.test.query-not
+               'datascript.test.query-aggregates
+               'datascript.test.query-or
+               'datascript.test.storage))
